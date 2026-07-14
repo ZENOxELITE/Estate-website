7e4ff05cf5fe9337b486382b349010ad63e326eb
Muneer Ahmed <156942752+ZENOxELITE@users.noreply.github.com>
2026-07-15 03:14:42 +0500

    Update ALLOWED_HOSTS for production settings

--- estate_agency/settings.py
+++ estate_agency/settings.py
@@ -15,8 +15,11 @@ SECRET_KEY = 'django-insecure-your-secret-key-change-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    "estate-website-fzs6.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 # Application definition
 
